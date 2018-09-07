664accbc65e0552beb5af92a680377079011963e
Igor <igorcbrito96@gmail.com>
2018-09-07 10:16:45 -0300

    Corrigida a questao 5

--- questao5.c
+++ questao5.c
@@ -1,46 +1,4 @@
-/*Questão 5
+Questão 5
 
-  R: a)20; b)26.5; c)P; d)e; e)P; f)e; g)t; h)31; i)45; j)27; l)31: m)45; n)27;
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#include <stdio.h>
-
-int main()
-{
-    int    valor;
-      int   *p1;
-      float  temp;
-      float *p2;
-      char   aux;
-      char  *nome = "Ponteiros";
-      char  *p3;
-      int    idade;
-      int    vetor[3];
-      int   *p4;
-      int   *p5;
-
-      temp = 26.5;
-        p2 = &temp;
-        *p2 = 29.0;
-        printf("%.1f \n", temp);
-
-
-}
+  R: a)20; b)29.0; c)P; d)e; e)P; f)e; g)t; h)31; i)45; j)27; l)31: m)45; n)27;
 
