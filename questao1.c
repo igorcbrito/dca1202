44ee77bf0c656b8fd94c2e436c6ab00c4650dba8
Igor <igorcbrito96@gmail.com>
2018-09-10 20:23:48 -0300

    Adicionadas as questoes 1, 19 e 20

---
+++ questao1.c
@@ -0,0 +1,6 @@
+Questão 1
+
+R: Sabendo que ponteiros guardam endereços de memória, é possível por exemplo passar um ponteiro como
+parâmetro de uma função e alterar o valor de um determinado endereço de memória, podendo assim ser reaproveitado em outras
+funções. Por consequência, o uso de ponteiros abre uma brecha para diversas funcionalidades dentro do programa que se queira criar.
+O uso de ponteiros também é importante para se obter maior eficiêcia.
